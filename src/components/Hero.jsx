2294b3290f20aceb647ea6a6080e93cf2c26bc8a
Kwaku Louis <bonglamgh@gmail.com>
2023-02-20 15:56:05 +0000

    worked on the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,39 +5,54 @@ import Googleplay from "../images/googleplayjipa.png";
 import Appstore from "../images/applestore.png";
 
 
+
+
+
 const Hero = () => {
   return (
 
     
-    <div className={`${styles.flexStart} ${styles.paddingX}  bg-slate-200`}>
+    <div className={`${styles.flexStart} ${styles.paddingX}   bg-slate-200`}>
       <div>
       <section  className={`flex md:flex-row flex-col `}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 `}>
+      <div className="flex  flex-row items-center pt-20 md:pt-32 py-[6px] px-4  rounded-[10px] mb-6">
+          {/* <img src={discount} alt="discount" className="w-[32px] h-[32px]" /> */}
        
+          <p className={`${styles.paragraph} text-sm ml-12`}>
+            <p className="bg-white md:py-1 py-1 rounded-3xl md:rounded-3xl ">&nbsp; <span className="bg-rose-200  md:px-2 px-1   rounded-3xl"> Latest</span> Loans  in 30 minutes&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+          </p>
+        </div>
 
-        <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1  text-center md:text-left md:text-[70px] pt-24 md:pt-0  font-bold md:px-16 ss:text-[72px] text-[30px] text-[#080357]    ss:leading-[100.8px] leading-[39px] md:leading-[75px]">
-            The Best Smart   <br className="sm:block hidden" />{" "}
-            <span className="text-gradient"> Digital Loans <span className="md:hidden"> Provider.</span></span>{" "}
+
+        <div className="flex flex-row  justify-between items-center w-full">
+         
+     
+          <h1 className="flex-1 font-poppins  text-center md:text-left md:text-[70px]   md:px-16 ss:text-[72px] text-[30px] text-[#080357]    ss:leading-[100.8px] leading-[39px] md:leading-[75px]">
+
+            <span> The Best Smart </span>  <br className="sm:block  hidden" />{" "}
+           
+            <span > Digital Loans <span className="md:hidden"> Provider.</span></span>{" "}
           </h1>
         
         </div>
        
-        <h1 className="font-poppins hidden md:block md:px-16 font-bold ss:text-[68px] text-[70px] text-[#080357]   ss:leading-[100.8px] leading-[75px] w-full">
+        <h1 className=" hidden md:block md:px-16  ss:text-[68px] text-[70px] text-[#080357]   ss:leading-[100.8px] leading-[75px] w-full">
           Provider.
         </h1>
-        <p  className={`${styles.paragraph}   max-w-[470px] text-center md:text-left pt-2 md:px-16 md:pt-3 text-slate-900 font-bold text-xl mt-5`}>
+      
+        <p  className={`${styles.paragraph}   max-w-[470px] text-center md:text-left   md:px-16 md:pt-3 text-slate-900  text-xl mt-5`}>
 
         Download the JIPA app to experience the ease and speed of our loan application process.
         </p>
         {/* Download buttons */}
-        <div className=" flex md:pl-16 pt-10 md:pt-5">
+        <div className=" flex md:pl-16 pl-10 pt-4 md:mb-20 md:pt-5">
           <div>
-          <img className="w-48 pt-3 md:pt-4" alt="googleplay" src={Googleplay}/>
+          <img className="w-32 md:w-40 pt-2 md:pt-4" alt="googleplay" src={Googleplay}/>
 
           </div>
           <div>
-          <img className="w-56" alt="googleplay" src={Appstore}/>
+          <img className="w-36  md:pt-1 md:w-48" alt="googleplay" src={Appstore}/>
 
           </div>
           
